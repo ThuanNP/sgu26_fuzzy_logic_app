36eab18c57fe8687c4ed29e2545ebab06b359fa9
Nguyễn Phước Thuận <15880525+ThuanNP@users.noreply.github.com>
2026-05-28 08:48:37 +0700

    Add new PDF resources for Fuzzy Logic and Control Systems exercises
    - Added "07 FuzzyLogic - Giai bai toan dieu khien mo.pdf" to the Material/Bai03 directory.
    - Added "BaiTap3 - He dieu khien mo.pdf" to the Material/Bai03 directory.

---
+++ Bai00/ex3_circle.py
@@ -0,0 +1,6 @@
+print("Tính chu vi hình tròn")
+r = int(input("Mời ban nhập bán kính hình tròn: "))
+pi = 3.14
+cv = 2 * pi * r
+dt = pi * r * r
+print("Chu vi hình tròn là: %.2f, diện tích hình tròn là: %.2f" % (cv, dt))
